7d22c65069dc05c4002004438ee6dffd45bf7dfd
Joseph Friesen <friesen.josephc@gmail.com>
2018-08-17 09:45:39 -0700

    Add logic to fade in appropriate output text based on the user entering a nonnegative integer, a float, a negative integer, or NaN.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 // BUSINESS-SIDE LOGIC //
 
-
+function testInt(value) {
+  if (value % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 
@@ -21,8 +27,27 @@ $(document).ready(function() {
   $("form#gimme-that-number").submit(function(event) {
     event.preventDefault();
 
-    var userNumber = $("#user-number").val();
-    console.log(userNumber);
+    var userInput = Number($("#user-number").val());
+
+    if (isNaN(userInput)) {
+      $("#number-list").fadeOut();
+      $("#not-an-int").fadeOut();
+      $("#not-a-number").fadeIn();
+    }
+    else if (testInt(userInput) === false || userInput < 0) {
+      console.log(userInput);
+      $("#number-list").fadeOut();
+      $("#not-a-number").fadeOut();
+      $("#not-an-int").fadeIn();
+    }
+    else if (testInt(userInput) === true && userInput >= 0) {
+      $("#not-a-number").fadeOut();
+      $("#not-an-int").fadeOut();
+      $("#number-list").fadeIn();
+      $("#put-number-here").text(userInput);
+    }
+
+
   });
 
 });
